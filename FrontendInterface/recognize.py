a9ebfa037cc3a420a809a9474d1b62d6633fd8cd
Vinny <k_yang6@uncg.edu>
2018-10-06 01:32:50 -0400

    changed code inside recognize.py, now it has the correct path to detect wav file

--- FrontendInterface/recognize.py
+++ FrontendInterface/recognize.py
@@ -4,7 +4,7 @@ print(recognition.__version__)
 
 recognizer = recognition.Recognizer();
 
-harvard = recognition.AudioFile("C:/Users/vince/PycharmProjects/spechRecgonizion/venv/Lib/python-speech-recognition-master/audio_files\harvard.wav");
+harvard = recognition.AudioFile("harvard.wav");
 
 with harvard as source:
 
