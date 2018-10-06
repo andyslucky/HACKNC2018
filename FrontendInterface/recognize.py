f6478372df4254e9e4bdb87c6cc9ebba4772d6e6
Vinny <k_yang6@uncg.edu>
2018-10-06 13:19:40 -0400

    finished recognize.py created temperary porter_stem.py

--- FrontendInterface/recognize.py
+++ FrontendInterface/recognize.py
@@ -1,49 +1,52 @@
-import threading
 import datetime
-
 import speech_recognition as recognition
-from speech_recognition import AudioFile
 
-start_time = datetime.datetime.now()
-start_time_arr = str(start_time).split(" ")
-start_time_current = start_time_arr[1].split(":")
-start_time_second = start_time_current[2]
-print("Start: " + str(start_time))
 print(recognition.__version__)
 
-recognizer = recognition.Recognizer();
 
-harvard: AudioFile = recognition.AudioFile("harvard.wav");
+def time_displayer(_string):
+    current_time = datetime.datetime.now()
+    print(_string + str(current_time))
 
-lock = threading.Lock()
+
+def create_instance():
+    recognizer = recognition.Recognizer();
+    microphone = recognition.Microphone();
+    with microphone as source:
+        print("Say Something: ")
+        audio: recognition.AudioData = recognizer.listen(source, phrase_time_limit=5)
+        print(type(audio))
+        parse_response = audio_recognizer(audio, recognizer)
+
+    return parse_response
 
 
-def audio_helper(_audio, _lock):
+def audio_recognizer(_audio, _recognizer):
     """
     This function is a helper function to parse the speech within a selected period.
     This function is also thread safe.
+    :param _recognizer: recognizer
     :param _audio: audio file
     :param _lock: thread lock
     :return: raw string acquired from audio file
     """
-    # Thread lock will stop the race condition from multiple thread trying to access one audio file.
-    with _lock:
-        audio_helper_speech = recognizer.recognize_sphinx(_audio)
+    if not isinstance(_audio, recognition.AudioData):
+        raise TypeError("'_audio' must be 'AudioFile' instance")
+    if not isinstance(_recognizer, recognition.Recognizer):
+        raise TypeError("'_recognizer' must be 'recognition.Recognizer()' instance")
 
-    return audio_helper_speech
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+    try:
+        response["transcription"] = _recognizer.recognize_google(_audio)
+    except recognition.RequestError:
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except recognition.UnknownValueError:
+        response["error"] = "Unable to recognize speech"
 
+    return response
 
-source: AudioFile
-with harvard as source:
-    print("file duration is:" + str(source.DURATION))
-    audio = recognizer.record(source, duration=source.DURATION)
-    speech = audio_helper(audio, lock)
-    print(speech.split(" "))
-
-
-end_time = datetime.datetime.now()
-end_time_arr = str(end_time).split(" ")
-end_time_current = end_time_arr[1].split(":")
-end_time_second = end_time_current[2]
-print("End: " + str(end_time))
-print(float(end_time_second) - float(start_time_second))
