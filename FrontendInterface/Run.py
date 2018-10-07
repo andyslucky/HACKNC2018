afc84c385ff18e539877229f3ffff98a8e6b13c5
Vinny <k_yang6@uncg.edu>
2018-10-06 21:53:14 -0400

    Modification on UserInterface

--- FrontendInterface/Run.py
+++ FrontendInterface/Run.py
@@ -1,27 +1,3 @@
-import Langprocessor
 import UserInterface
-import Input_io
-import Server
-
-# requested_input = Input_io.create_instance()
-#
-# commands = Langprocessor.process_sentence(requested_input["transcription"])
-# server = None
-# def read(client,response):
-#     print(response)
-# def connected(addr):
-#     print(str(addr)+"connected")
-#     server.setState(addr,0)
-#     server.getState(addr)
-# def disconected(addr):
-#     print(str(addr)+"disconnected")
-#
-# server = Server.Server(read,connected,disconected)
-# server.start()
 
 ui = UserInterface.UserInterface()
-
-
-# if commands.get("error") is not None:
-#     print("command has error message: " + commands.get("error"))
-#changes
